2f6c5d2c1c774b8f1598d541aaaf4a38fccc2695
looky26 <vajearvin03@gmail.com>
2023-06-18 14:25:08 +0800

    udpated next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        esmExternals: true,
+        appDir: true,
+        externalDir: true,
+        esmExternals:'loose',
       },
 }
 
